2d2f2ddc410bb33abba93da973cb6f215a6bfe70
Hccake <chengbohua@foxmail.com>
2022-06-12 18:22:21 +0800

    :sparkles: (footer-view) 添加 footerStyle props

--- pro-components/layout/Footer.tsx
+++ pro-components/layout/Footer.tsx
@@ -1,9 +1,8 @@
 import { Layout } from 'ant-design-vue'
 import GlobalFooter from './components/GlobalFooter'
 
-import type { CSSProperties } from 'vue'
+import type { CSSProperties, PropType } from 'vue'
 import type { WithFalse } from './types'
-import type { FunctionalComponent } from 'vue'
 import type { VueNodeOrRender } from '#/types'
 
 export type FooterProps = {
@@ -17,14 +16,39 @@ export type FooterProps = {
   >
   copyright?: WithFalse<string>
   prefixCls?: string
+  footerStyle?: CSSProperties
 }
 
-const FooterView: FunctionalComponent<FooterProps> = (props: FooterProps, { slots, attrs }) => (
-  <Layout.Footer class={attrs.class} style={{ padding: 0, ...(attrs.style as CSSProperties) }}>
-    <GlobalFooter links={props.links} prefixCls={props.prefixCls} copyright={props.copyright}>
-      {slots}
-    </GlobalFooter>
-  </Layout.Footer>
-)
+const footerViewProps = {
+  links: {
+    type: [Object, Boolean] as PropType<FooterProps['links']>,
+    default: undefined
+  },
+  copyright: {
+    type: [String, Boolean] as PropType<FooterProps['copyright']>,
+    default: undefined
+  },
+  prefixCls: String as PropType<FooterProps['prefixCls']>,
+  footerStyle: Object as PropType<FooterProps['footerStyle']>
+}
+
+const FooterView = defineComponent({
+  name: 'FooterView',
+  props: footerViewProps,
+  setup(props, { slots, attrs }) {
+    return () => (
+      <Layout.Footer class={attrs.class} style={{ padding: 0, ...(attrs.style as CSSProperties) }}>
+        <GlobalFooter
+          links={props.links}
+          prefixCls={props.prefixCls}
+          copyright={props.copyright}
+          style={{ ...(props.footerStyle as CSSProperties) }}
+        >
+          {slots}
+        </GlobalFooter>
+      </Layout.Footer>
+    )
+  }
+})
 
 export default FooterView
