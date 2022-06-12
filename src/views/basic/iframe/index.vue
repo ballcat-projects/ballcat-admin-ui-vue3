ef7300c4f0ebcc45818e32d69c2be3738b3e16f2
Hccake <chengbohua@foxmail.com>
2022-06-12 18:51:44 +0800

    :sparkles: (iframe) 完成 IframeView 组件

--- src/views/basic/iframe/index.vue
+++ src/views/basic/iframe/index.vue
@@ -1,11 +1,53 @@
 <template>
-  <div />
+  <a-spin
+    tip="Loading..."
+    size="large"
+    :spinning="iframeLoading"
+    wrapper-class-name="iframe-spin-wrapper"
+  >
+    <iframe :id="id" ref="iframeRef" :src="url" class="iframe" @load="hideLoading" />
+  </a-spin>
 </template>
 
-<script>
+<script setup lang="ts">
+const iframeRef = ref<HTMLIFrameElement>()
+
+const route = useRoute()
+const id = route.path
+const url = route.meta.target
+
+const iframeLoading = ref(true)
+
+function iframeResize() {
+  const iframe = unref(iframeRef)
+  if (iframe) {
+    const clientHeight = document.getElementsByClassName('ant-layout-content')[0].clientHeight - 1
+    iframe.style.height = `${clientHeight}px`
+  }
+}
+
+onMounted(() => window.addEventListener('resize', iframeResize))
+onUnmounted(() => window.removeEventListener('resize', iframeResize))
+
+const hideLoading = () => {
+  iframeLoading.value = false
+  iframeResize()
+}
+</script>
+
+<script lang="ts">
 export default {
   name: 'IframeView'
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.iframe {
+  width: 100%;
+  height: 100%;
+  border: 0;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: block;
+}
+</style>
