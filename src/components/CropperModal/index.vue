024e64b5fc402d427b6db258e86e435793b1abe6
Hccake <chengbohua@foxmail.com>
2022-06-21 08:47:57 +0800

    :sparkles: (cropper) 使用 @ballcat/vue-cropper 组件重写

--- src/components/CropperModal/index.vue
+++ src/components/CropperModal/index.vue
@@ -4,52 +4,115 @@
     :visible="visible"
     :mask-closable="false"
     :confirm-loading="confirmLoading"
-    :width="800"
+    :width="720"
+    :body-style="{ padding: '16px 24px' }"
     :footer="null"
-    @cancel="close"
+    @cancel="closeModal"
   >
-    <a-row>
-      <a-col :xs="24" :md="12" :style="{ height: '350px' }">
+    <a-row :gutter="16">
+      <a-col :xs="24" :md="18" :style="{ height: '320px' }">
         <vue-cropper
+          v-show="imageSrc"
           ref="cropperRef"
-          :img="options.img"
-          :info="true"
-          :auto-crop="options.autoCrop"
-          :auto-crop-width="options.autoCropWidth"
-          :auto-crop-height="options.autoCropHeight"
-          :fixed-box="options.fixedBox"
-          @real-time="realTime"
+          img-cross-origin="anonymous"
+          alt="avatar"
+          drag-mode="move"
+          :src="imageSrc"
+          :aspect-ratio="1"
+          style="height: 100%"
+          preview=".cropper-modal-preview"
+        />
+        <div
+          v-if="!imageSrc"
+          class="cropper-container cropper-bg"
+          style="width: 100%; height: 320px"
         />
       </a-col>
-      <a-col :xs="24" :md="12" :style="{ height: '350px' }">
-        <div class="avatar-upload-preview">
-          <img v-if="previews.url" :src="previews.url" :style="previews.img" alt="previewImg" />
-        </div>
+      <a-col>
+        <div class="cropper-modal-preview" />
+        <div class="cropper-modal-preview circle" style="margin-top: 20px" />
       </a-col>
     </a-row>
     <a-row justify="center">
       <a-col>
-        <div style="margin-top: 20px">
+        <div style="margin-top: 20px" class="cropper-modal-buttons">
           <a-space :size="20">
-            <a-upload name="file" :before-upload="beforeUpload" :show-upload-list="false">
-              <a-button>
-                <UploadOutlined />
-                选择图片
+            <a-button-group>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.zoom(0.1))">
+                <template #icon>
+                  <zoom-in-outlined />
+                </template>
               </a-button>
-            </a-upload>
-            <a-button @click="() => cropperRef.changeScale(1)">
-              <PlusOutlined />
-            </a-button>
-            <a-button @click="() => cropperRef.changeScale(-1)">
-              <MinusOutlined />
-            </a-button>
-            <a-button @click="() => cropperRef.rotateLeft()">
-              <UndoOutlined />
-            </a-button>
-            <a-button @click="() => cropperRef.rotateRight()">
-              <RedoOutlined />
-            </a-button>
-            <a-button type="primary" @click="upload">保存</a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.zoom(-0.1))">
+                <template #icon>
+                  <zoom-out-outlined />
+                </template>
+              </a-button>
+            </a-button-group>
+
+            <a-button-group>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.move(-10, 0))">
+                <template #icon>
+                  <arrow-left-outlined />
+                </template>
+              </a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.move(10, 0))">
+                <template #icon>
+                  <arrow-right-outlined />
+                </template>
+              </a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.move(0, -10))">
+                <template #icon>
+                  <arrow-up-outlined />
+                </template>
+              </a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.move(0, 10))">
+                <template #icon>
+                  <arrow-down-outlined />
+                </template>
+              </a-button>
+            </a-button-group>
+
+            <a-button-group>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.rotate(-45))">
+                <template #icon>
+                  <rotate-left-outlined />
+                </template>
+              </a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.rotate(45))">
+                <template #icon>
+                  <rotate-right-outlined />
+                </template>
+              </a-button>
+            </a-button-group>
+
+            <a-button-group>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.flipX())">
+                <template #icon>
+                  <swap-outlined />
+                </template>
+              </a-button>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.flipY())">
+                <template #icon>
+                  <swap-outlined :rotate="90" />
+                </template>
+              </a-button>
+            </a-button-group>
+
+            <a-button-group>
+              <a-button type="primary" @click="applyWhenHasImage(() => cropperRef?.reset())">
+                <template #icon>
+                  <sync-outlined />
+                </template>
+              </a-button>
+              <a-upload name="file" :before-upload="beforeUpload" :show-upload-list="false">
+                <a-button type="primary">
+                  <template #icon><upload-outlined /></template>
+                </a-button>
+              </a-upload>
+            </a-button-group>
+
+            <a-button type="primary" @click="applyWhenHasImage(() => upload())">保存</a-button>
           </a-space>
         </div>
       </a-col>
@@ -58,12 +121,14 @@
 </template>
 
 <script setup lang="ts">
-import { VueCropper } from 'vue-cropper'
-import 'vue-cropper/dist/index.css'
-import { message } from 'ant-design-vue'
+import VueCropper from '@ballcat/vue-cropper'
+import type { VueCropperInstance } from '@ballcat/vue-cropper'
+import 'cropperjs/dist/cropper.css'
 import type { UploadProps } from 'ant-design-vue'
 import type { ApiResult } from '@/api/types'
 import type { FileObject } from '@/components/CropperModal/types'
+import { message } from 'ant-design-vue'
+import { useModal } from '@/hooks/modal'
 
 const props = defineProps<{
   uploadProcessor: (file: FileObject, params: any) => Promise<ApiResult>
@@ -73,34 +138,21 @@ const emits = defineEmits<{
   <T>(e: 'ok', data: ApiResult, extra?: T): void
 }>()
 
-const cropperRef = ref()
-const options = reactive({
-  // img: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  img: '',
-  autoCrop: true,
-  autoCropWidth: 200,
-  autoCropHeight: 200,
-  fixedBox: true
-})
+const cropperRef = ref<VueCropperInstance>()
 
-type CropperData = {
-  url?: string
-  img?: CSSStyleRule
-}
-const previews = ref<CropperData>({})
-const realTime = (data: CropperData) => {
-  options.img && (previews.value = data)
+const imageSrc = ref<string>('')
+
+const { visible, openModal, closeModal } = useModal()
+
+/* 执行方法前先检查是否存在图片 */
+function applyWhenHasImage(func: () => void) {
+  if (imageSrc.value && cropperRef.value) {
+    func()
+  } else {
+    message.error('请先上传一张图片！')
+  }
 }
 
-let extra: any
-const visible = ref(false)
-const open = (img: string, info?: any) => {
-  previews.value = {}
-  extra = info
-  options.img = img
-  visible.value = true
-}
-const close = () => (visible.value = false)
 const confirmLoading = ref(false)
 
 let currentFilename = ''
@@ -110,7 +162,7 @@ const beforeUpload: UploadProps['beforeUpload'] = file => {
   // 转化为base64
   reader.readAsDataURL(file)
   reader.onload = () => {
-    options.img = reader.result as string
+    imageSrc.value = reader.result as string
   }
   currentFilename = file.name
   // 转化为blob
@@ -118,22 +170,40 @@ const beforeUpload: UploadProps['beforeUpload'] = file => {
   return false
 }
 
+let extra: any
+
 // 上传图片（点击上传按钮）
 const upload = () => {
+  const croppedCanvas = cropperRef.value?.getCroppedCanvas()
+  if (croppedCanvas) {
+    try {
+      croppedCanvas.toBlob((data: Blob | null) => {
+        if (!data) {
+          message.error('裁剪图片失败')
+          return
+        }
+        props.uploadProcessor({ data: data, name: currentFilename }, extra).then(data => {
+          confirmLoading.value = false
+          message.success('上传图片成功')
+          emits('ok', data)
+          close()
+        })
+      })
+    } catch (e) {
+      message.error('裁剪图片异常，请检查图片源是否不允许跨域加载')
+    }
+  }
+
   // TODO 图片上传有上传文件和Base64字符串两种方式，暂时为做Base64方式的处理
-  confirmLoading.value = true
-  cropperRef.value.getCropBlob((data: Blob) => {
-    props.uploadProcessor({ data: data, name: currentFilename }, extra).then(data => {
-      confirmLoading.value = false
-      message.success('上传图片成功')
-      emits('ok', data)
-      close()
-    })
-  })
+  // confirmLoading.value = true
 }
 
 defineExpose({
-  open
+  open: (img: string, info?: any) => {
+    extra = info
+    imageSrc.value = img || ''
+    openModal()
+  }
 })
 </script>
 
@@ -144,18 +214,35 @@ export default {
 </script>
 
 <style scoped lang="less">
-.avatar-upload-preview {
-  position: absolute;
-  top: 50%;
-  transform: translate(50%, -50%);
-  width: 180px;
-  height: 180px;
-  box-shadow: 0 0 4px #ccc;
+.cropper-modal-preview {
+  width: 145px;
+  height: 145px;
+  border-style: solid;
+  border-color: #b5b5b5;
+  border-width: 1px;
+  background-color: #f7f7f7;
   overflow: hidden;
 
   img {
     width: 100%;
     height: 100%;
+  }
+
+  &.circle {
+    width: 150px;
+    height: 150px;
+    border-radius: 85px;
+  }
+}
+
+.cropper-modal-buttons {
+  .ant-btn {
+    height: 40px;
+  }
+
+  .ant-btn-icon-only {
+    width: 40px;
+    height: 40px;
   }
 }
 </style>
