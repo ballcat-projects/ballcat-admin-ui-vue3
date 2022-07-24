8a8ac80909b28607e5cd02a812e6ec10240d74e8
Hccake <chengbohua@foxmail.com>
2022-07-24 23:51:16 +0800

    :sparkles: (http) 添加默认的请求配置，使用 QS 进行参数格式化
    - 数组的格式化方式变为 repeat, 默认是 brackets
    - 对于值为空字符串的参数不再进行提交

--- src/utils/axios/http-client.ts
+++ src/utils/axios/http-client.ts
@@ -1,6 +1,24 @@
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
 import axios from 'axios'
 import type { HttpClientConfig } from '@/utils/axios/types'
+import qs from 'qs'
+
+// HttpClient 默认的请求配置
+const DefaultRequestConfig = {
+  paramsSerializer: function (params: any) {
+    return qs.stringify(params, {
+      // 数组的格式化方式为重复参数，例如 { a: ['1', '2']} => a=1&a=2
+      arrayFormat: 'repeat',
+      filter: (prefix, value) => {
+        // 空字符串不进行提交
+        if (typeof value == 'string' && value.length === 0) {
+          return
+        }
+        return value
+      }
+    })
+  }
+}
 
 export class HttpClient {
   private axiosInstance: AxiosInstance
@@ -30,8 +48,8 @@ export class HttpClient {
   }
 
   request<T = any, D = any>(config: AxiosRequestConfig<D>) {
-    // const mergeConfig = Object.assign({}, defaultConfig, config)
-    return this.axiosInstance.request<void, T, D>(config)
+    const mergeConfig = Object.assign({}, DefaultRequestConfig, config)
+    return this.axiosInstance.request<void, T, D>(mergeConfig)
   }
 
   get<T = any, D = any>(url: string, config?: AxiosRequestConfig<D>) {
