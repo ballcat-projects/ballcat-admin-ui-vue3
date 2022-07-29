554ce7600c1051740ba9c4824e43d289d67ef5a7
Hccake <chengbohua@foxmail.com>
2022-07-29 23:32:25 +0800

    :zap: 图片域名使用 env 配置

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,8 @@ interface ImportMetaEnv {
   readonly VITE_API_TIME_OUT: number
   // 密码加密密钥, 16 位字符串
   readonly VITE_PASSWORD_SECRET_KEY: string
+  // 图片域名
+  readonly VITE_IMAGE_DOMAIN: string
 }
 
 interface ImportMeta {
--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -1,7 +1,35 @@
-export const fileAbsoluteUrl = (relativePath: string) => {
+export const fileAbsoluteUrl = (
+  relativePath: string,
+  fileDomain = import.meta.env.VITE_IMAGE_DOMAIN
+) => {
   if (!relativePath || relativePath.length === 0) return
-  const fileDomain = 'https://hccake-img.oss-cn-shanghai.aliyuncs.com'
   return relativePath.startsWith('/')
     ? `${fileDomain}${relativePath}`
     : `${fileDomain}/${relativePath}`
+}
+
+/**
+ * Convert BASE64 to BLOB
+ * @param base64Image Pass Base64 image data to convert into the BLOB
+ */
+export function convertBase64ToBlob(base64Image: string) {
+  // Split into two parts
+  const parts = base64Image.split(';base64,')
+
+  // Hold the content type
+  const imageType = parts[0].split(':')[1]
+
+  // Decode Base64 string
+  const decodedData = window.atob(parts[1])
+
+  // Create UNIT8ARRAY of size same as row data length
+  const uInt8Array = new Uint8Array(decodedData.length)
+
+  // Insert all character code into uInt8Array
+  for (let i = 0; i < decodedData.length; ++i) {
+    uInt8Array[i] = decodedData.charCodeAt(i)
+  }
+
+  // Return BLOB image after conversion
+  return new Blob([uInt8Array], { type: imageType })
 }
