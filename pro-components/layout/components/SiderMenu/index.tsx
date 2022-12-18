efbe8a5cbc57f17bec6ad84aeb9cc2626541ec76
Hccake <chengbohua@foxmail.com>
2022-12-18 18:28:07 +0800

    :bug: 修复移动端菜单无法打开问题
    Closes gh-16

--- pro-components/layout/components/SiderMenu/index.tsx
+++ pro-components/layout/components/SiderMenu/index.tsx
@@ -14,43 +14,56 @@ const SiderMenuWrapper = defineComponent({
     // TODO 计算 flatMenuKeys
 
     // 当切换设备为手机时，会自动折叠菜单
-    watchEffect(() => {
-      if (props.isMobile === true) {
-        props.onCollapse?.(true)
-      }
-    })
+    watch(
+      () => props.isMobile,
+      () => {
+        if (props.isMobile == true) {
+          props.onCollapse?.(true)
+        }
+      },
+      { immediate: true }
+    )
 
     if (props.hide) {
       return null
     }
 
+    const drawerVisible = ref(false)
+    watchEffect(() => {
+      // @ts-ignore
+      drawerVisible.value = !props.collapsed
+    })
+
     // @ts-ignore
     return () =>
       props.isMobile ? (
-        <Drawer
-          visible={!props.collapsed}
-          placement="left"
-          class={[`${props.prefixCls}-drawer-sider`, attrs.class]}
-          onClose={() => props.onCollapse?.(true)}
-          style={{
-            padding: 0,
-            height: '100vh',
-            ...(attrs.style as CSSProperties)
-          }}
-          getContainer={props.getContainer}
-          width={props.siderWidth}
-          bodyStyle={{ height: '100vh', padding: 0, display: 'flex', flexDirection: 'row' }}
-        >
-          <SiderMenu
-            {...props}
-            // @ts-ignore
-            class={[`${props.prefixCls}-sider`, attrs.class]}
-            collapsed={props.isMobile ? false : props.collapsed}
-            splitMenus={false}
+        <>
+          <Drawer
+            visible={drawerVisible.value}
+            placement="left"
+            class={[`${props.prefixCls}-drawer-sider`, attrs.class]}
+            onClose={() => props.onCollapse?.(true)}
+            style={{
+              padding: 0,
+              height: '100vh',
+              ...(attrs.style as CSSProperties)
+            }}
+            closable={false}
+            getContainer={props.getContainer}
+            width={props.siderWidth}
+            bodyStyle={{ height: '100vh', padding: 0, display: 'flex', flexDirection: 'row' }}
           >
-            {slots}
-          </SiderMenu>
-        </Drawer>
+            <SiderMenu
+              {...props}
+              // @ts-ignore
+              class={[`${props.prefixCls}-sider`, attrs.class]}
+              collapsed={props.isMobile ? false : props.collapsed}
+              splitMenus={false}
+            >
+              {slots}
+            </SiderMenu>
+          </Drawer>
+        </>
       ) : (
         <SiderMenu
           class={[`${props.prefixCls}-sider`, attrs.class]}
