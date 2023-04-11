1d8d1e72a502cad0de92b828dff283a90fcd01bf
Hccake <chengbohua@foxmail.com>
2023-04-11 20:41:20 +0800

    :sparkles: 添加 DictTagGroup 组件，方便回显一组标签

---
+++ src/components/Dict/display/DictTagGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-space :size="5">
+    <dict-tag
+      v-for="dictValue in props.dictValues"
+      :key="dictValue"
+      :dict-code="props.dictCode"
+      :value="dictValue"
+    />
+  </a-space>
+</template>
+
+<script setup lang="ts">
+import type { DictValue } from '@/api/system/dict/types'
+import DictTag from '@/components/Dict/display/DictTag.vue'
+
+const props = defineProps<{
+  dictCode: string
+  dictValues: DictValue[]
+}>()
+</script>
+
+<script lang="ts">
+export default {
+  name: 'DictTagGroup'
+}
+</script>
+
+<style scoped></style>
--- src/components/Dict/index.ts
+++ src/components/Dict/index.ts
@@ -5,3 +5,5 @@ export { default as DictRadioGroup } from './group/DictRadioGroup.vue'
 export { default as DictBadge } from './display/DictBadge.vue'
 export { default as DictTag } from './display/DictTag.vue'
 export { default as DictText } from './display/DictText.vue'
+
+export { default as DictTagGroup } from './display/DictTagGroup.vue'
