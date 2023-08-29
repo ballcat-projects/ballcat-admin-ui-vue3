c669dcaa3b4fdeb4ea5d181870fbe0cc4876d096
Hccake <chengbohua@foxmail.com>
2023-08-29 19:30:22 +0800

    :bug: 修复 DictSelect onChange 警告问题

--- src/components/Dict/group/DictSelect.vue
+++ src/components/Dict/group/DictSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-select v-bind="props" :filter-option="filterOption" @change="onChange">
+  <a-select v-bind="innerProps" :filter-option="filterOption">
     <a-select-option
       v-for="dictItem in dictItems"
       :key="dictItem.id"
@@ -15,7 +15,7 @@
 <script setup lang="ts">
 import type { DictValue } from '@/api/system/dict/types'
 import { useDict } from '@/components/Dict/use-dict'
-import type { SelectValue } from 'ant-design-vue/es/select'
+import type { DefaultOptionType, SelectValue } from 'ant-design-vue/es/select'
 import { type DictComponentProps, dictSelectProps } from '@/components/Dict/types'
 
 const props = defineProps(dictSelectProps())
@@ -24,9 +24,13 @@ const emits = defineEmits<{
   (e: 'update:value', selectedValue: DictValue | DictValue[]): void
 }>()
 
-const onChange = (selectedValue: SelectValue) => {
-  emits('update:value', selectedValue as DictValue | DictValue[])
-}
+const innerProps = computed(() => ({
+  ...props,
+  onChange: (selectedValue: SelectValue, option: DefaultOptionType | DefaultOptionType[]) => {
+    emits('update:value', selectedValue as DictValue | DictValue[])
+    props.onChange?.(selectedValue, option)
+  }
+}))
 
 function defaultFilterOption(input: string, option: any) {
   return option.name.toLowerCase().indexOf(input.toLowerCase()) >= 0
