5b990e2868288d89510c964530b21924370c5c2b
Hccake <chengbohua@foxmail.com>
2024-01-20 13:30:09 +0800

    :art: (i18n) 优化关闭国际化功能时 antd 组件默认语言为中文

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import { useI18n } from 'vue-i18n'
 import { enableI18n } from '@/config'
 import type { Locale } from 'ant-design-vue/es/locale-provider'
 import type { Ref } from 'vue'
+import dayjs from 'dayjs'
 
 let antdLocal: Ref<Locale>
 
@@ -19,5 +20,13 @@ if (enableI18n) {
   antdLocal = computed<Locale>(() => {
     return i18n.getLocaleMessage(i18nStore.language)?.antdLocale as Locale
   })
+} else {
+  // 未开启国际化，默认使用中文
+  const modules = import.meta.glob('@/locales/lang/default-local-import.ts', { eager: true })
+  for (const path in modules) {
+    // @ts-ignore
+    antdLocal = modules[path].default
+  }
+  dayjs.locale('zh-cn')
 }
 </script>
---
+++ src/locales/lang/default-local-import.ts
@@ -0,0 +1,4 @@
+import localeValues from 'ant-design-vue/es/locale/zh_CN'
+import 'dayjs/locale/zh-cn'
+
+export default localeValues
